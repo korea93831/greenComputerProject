04a4fb2e4c274a9ca0034813f7c73db1ce2eaa49
minwoo.cha <ds2cmw@naver.com>
2024-05-10 14:37:16 +0900

    5월 10일 1-1 수정

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Introduction from '../views/Introduction.vue'
 
 const routes = [
   {
@@ -14,6 +15,12 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+
+  {
+    path: '/Introduction',
+    name: 'Introduction',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Introduction.vue')
   }
 ]
 
---
+++ client/src/views/Introduction.vue
@@ -0,0 +1,33 @@
+<template>
+    <div style="background-image: url('/client/src/images/tree8175062640.jpg');">
+      <!-- 내용을 여기에 추가하세요 -->
+    </div>
+  </template>
+  
+  <script>
+  import { Navbar, NavbarBrand, NavbarToggle, NavbarNav, NavItem, Collapse, BIcon } from 'bootstrap-vue';
+  
+  export default {
+    components: {
+
+    }
+  }
+  </script>
+  
+  <style>
+  .fullscreen-background {
+    /* 배경 이미지 설정 */
+    
+    /* 배경 이미지를 화면에 맞게 조정 */
+    background-size: cover;
+    background-position: center;
+    /* 전체 화면으로 확장 */
+    width: 100vw;
+    height: 100vh;
+    /* 모든 컨텐츠를 가운데로 정렬 */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  </style>
+  
